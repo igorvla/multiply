ed3ad38e704fd5cfffde1298fb2ad49aa41b6bc9
igorvla <16.may.1997@mail.ru>
2019-10-01 22:24:01 +0300

    init: start task Multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+    var firstInt = parseInt(first);
+    var secondInt = parseInt(second);
+
+
+    var result2 = firstInt * secondInt;
+
+    var result = String(result2);
+
+    return result;
 }
+
+
+
+
+
+/*var result2 = parseInt(first * second);
+
+var result = String(result2);
+
+return result;*/
+
+
+
+
+/*var firstInt = parseInt(first);
+  var secondInt = parseInt(second);
+
+  /*var result = firstInt * secondInt;*/
